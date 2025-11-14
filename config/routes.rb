5c46087fff2bcf66374283b06c8df39d4a12eaaf
juaninicolai <juan.nicolai@maersk.com>
2025-11-14 15:35:15 +0100

    add product route as root add unsubscribe controller and email link

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "products#index"
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
 end

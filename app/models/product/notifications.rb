1ace50fa0c24a905af47637f1cc3e1f13759f8f7
juaninicolai <juan.nicolai@maersk.com>
2025-11-14 15:17:12 +0100

    extract

---
+++ app/models/product/notifications.rb
@@ -0,0 +1,16 @@
+module Product::Notifications extends ActiveSupport::Concern
+  included do
+    has_many :subscribers, dependent: :destroy
+    after_update_commit :notify_subscribers, if: :back_in_stock?
+  end
+
+  def back_in_stock?
+    inventory_count_previously_was.zero? && inventory_count.positive?
+  end
+
+  def notify_subscribers
+    subscribers.each do |subscribers|
+      ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_layer
+    end
+  end
+end
